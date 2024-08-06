a873682459a3526b7f26e8c330ba54047c258bb6
Hyojoong,Kim <115703110+hn7093@users.noreply.github.com>
2024-08-06 13:29:52 +0900

    repeat/ 홈 : 태그,주제 불러오기 수정

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -62,7 +62,12 @@ function HomePage() {
 
                     const fetchAIQuestionData = async () => {
                         const response = await getAIQuestionData(getToken);
-                        setQuestionData(response.result);
+                        console.log('response: 2', response.result);
+                        setQuestionData({
+                            memberId: response.result.memberId,
+                            category: response.result.category,
+                            content: response.result.title
+                        });
                     };
 
                     // Fetch all necessary data
@@ -109,85 +114,90 @@ function HomePage() {
         const getToken = sessionStorage.getItem('token');
         if (getToken) {
             const response = await getAIQuestionData(getToken);
-            setQuestionData(response.result);
+            console.log('response: 3', response.result);
+            setQuestionData({
+                memberId: response.result.memberId,
+                category: response.result.category,
+                content: response.result.title
+            });
         }
     };
 
     return (
         <>
-        <Loading></Loading>
-        <S.Container>
-            <M.ModalContainer show={modalSwitch ? 'true' : undefined}>
-                {currentModal === 'MyDiary' ? (
-                    <MyDiaryModal setModalSwitch={setModalSwitch} diaryId={diaryId} />
-                ) : currentModal === 'OtherDiary' ? (
-                    <OtherDiaryModal setModalSwitch={setModalSwitch} />
-                ) : null}
-            </M.ModalContainer>
-            <S.TextDiv>
-                <h6>쓰담쓰담<br />하루의 끝<br />나의 마음일기</h6>
-                <p>오로지 나만을 위한 일기를 써보세요</p>
-            </S.TextDiv>
+            <Loading></Loading>
+            <S.Container>
+                <M.ModalContainer show={modalSwitch ? 'true' : undefined}>
+                    {currentModal === 'MyDiary' ? (
+                        <MyDiaryModal setModalSwitch={setModalSwitch} diaryId={diaryId} />
+                    ) : currentModal === 'OtherDiary' ? (
+                        <OtherDiaryModal setModalSwitch={setModalSwitch} />
+                    ) : null}
+                </M.ModalContainer>
+                <S.TextDiv>
+                    <h6>쓰담쓰담<br />하루의 끝<br />나의 마음일기</h6>
+                    <p>오로지 나만을 위한 일기를 써보세요</p>
+                </S.TextDiv>
 
-            <S.ProfileBox to='/login'>
-                <S.Circle>
-                    <img src={userData.profileImage} />
-                </S.Circle>
-                <S.ProfileText>
-                    <h6>{userData.username || '로그인 해주세요'}</h6>
-                    <p>
-                        {userData.ageGroup} / 
-                        {userData.keywordList.length > 0 
-                            ? userData.keywordList.map(keyword => ` #${keyword.category}`).join(' ') 
-                            : '키워드'}
-                    </p>
-                </S.ProfileText>
-            </S.ProfileBox>
+                <S.ProfileBox to='/login'>
+                    <S.Circle>
+                        <img src={userData.profileImage} />
+                    </S.Circle>
+                    <S.ProfileText>
+                        <h6>{userData.username || '로그인 해주세요'}</h6>
+                        <p>
+                            {userData.ageGroup} /
+                            {userData.keywordList.length > 0
+                                ? userData.keywordList.map(keyword => ` #${keyword.category}`).join(' ')
+                                : '키워드'}
+                        </p>
+                    </S.ProfileText>
+                </S.ProfileBox>
 
-            {token ? (
-                <S.QBox className="question">
-                    <h6>TODAY<br />QUESTION</h6>
-                    <h5># {questionData.category || 'AI 연동 중'}</h5>
-                    <p>Q. {questionData.content || 'AI 연동 중'}</p>
-                </S.QBox>
-            ) : (
-                <S.QBox className="noLogin">
-                    <h4>쓰담쓰담이 당신을 위로 할<br/>오늘의 일기 주제를 생성하고 있어요 :)</h4>
-                </S.QBox>
-            )}
-            
-            <S.ChangeButton onClick={handleChangeButtonClick}>주제 변경하기</S.ChangeButton>
+                {token ? (
+                    <S.QBox className="question">
+                        <h6>TODAY<br />QUESTION</h6>
+                        <h5># {questionData.category || 'AI 연동 중'}</h5>
+                        <p>Q. {questionData.content || 'AI 연동 중'}</p>
+                    </S.QBox>
+                ) : (
+                    <S.QBox className="noLogin">
+                        <h4>쓰담쓰담이 당신을 위로 할<br />오늘의 일기 주제를 생성하고 있어요 :)</h4>
+                    </S.QBox>
+                )}
 
-            <div style={{ gap: '14px' }}>
-                <S.DiaryButton className="free" onClick={handleWriteFreeDiaryClick}>
-                    <p><span>My Story</span><br /> 자유주제로<br />일기쓰기</p>
-                </S.DiaryButton>
-                <S.DiaryButton className="daily" onClick={handleWriteDiaryClick}>
-                    <p><span>My Story</span><br /> 지정주제로<br />일기쓰기</p>
-                </S.DiaryButton>
-            </div>
+                <S.ChangeButton onClick={handleChangeButtonClick}>주제 변경하기</S.ChangeButton>
 
-            <S.HistoryBox to="/user">
-                <h6>MY<br />HISTORY</h6>
-                <h5>나의 일기 기록 모아보기</h5>
-            </S.HistoryBox>
-            <S.StoryContainer>
-                <h3>
-                    쓰담쓰담<br />
-                    오늘의 스토리텔러는?
-                </h3>
-                {popularDiary.map(diary => (
-                    <StoryBox 
-                        key={diary.diaryId} 
-                        diary={diary} 
-                        onClick={() => handleStoryBoxClick(diary.diaryId)} 
-                    />
-                ))}
-                <S.MoreButton to="/diary">
-                    <h6>더 많은 일기 보기 &gt;</h6>
-                </S.MoreButton>
-            </S.StoryContainer>
-        </S.Container>
+                <div style={{ gap: '14px' }}>
+                    <S.DiaryButton className="free" onClick={handleWriteFreeDiaryClick}>
+                        <p><span>My Story</span><br /> 자유주제로<br />일기쓰기</p>
+                    </S.DiaryButton>
+                    <S.DiaryButton className="daily" onClick={handleWriteDiaryClick}>
+                        <p><span>My Story</span><br /> 지정주제로<br />일기쓰기</p>
+                    </S.DiaryButton>
+                </div>
+
+                <S.HistoryBox to="/user">
+                    <h6>MY<br />HISTORY</h6>
+                    <h5>나의 일기 기록 모아보기</h5>
+                </S.HistoryBox>
+                <S.StoryContainer>
+                    <h3>
+                        쓰담쓰담<br />
+                        오늘의 스토리텔러는?
+                    </h3>
+                    {popularDiary.map(diary => (
+                        <StoryBox
+                            key={diary.diaryId}
+                            diary={diary}
+                            onClick={() => handleStoryBoxClick(diary.diaryId)}
+                        />
+                    ))}
+                    <S.MoreButton to="/diary">
+                        <h6>더 많은 일기 보기 &gt;</h6>
+                    </S.MoreButton>
+                </S.StoryContainer>
+            </S.Container>
         </>
     );
 }
